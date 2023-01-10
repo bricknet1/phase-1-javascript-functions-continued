97776dfd32f5535db67baa1775ce37dfee236bce
Nick Johnson <nlj222@gmail.com>
2023-01-09 22:14:35 -0800

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+
+function saturdayFun(activity) {
+    if (activity === undefined) {activity = 'roller-skate'}
+    else {activity = activity}
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(work = "go to the office") {
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(symbol = "*") {
+    const innerFunction = function (thing = "special") {return `You are ${symbol}${thing}${symbol}!`};
+    return innerFunction;
+}
